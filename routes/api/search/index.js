09f1ed79ca792b9726063ec6864a92149917b9a6
tearsofphoenix <tearsofphoenix@icloud.com>
2017-11-20 18:13:42 +0800

    working on search

--- routes/api/search/index.js
+++ routes/api/search/index.js
@@ -7,28 +7,42 @@ const Comment = mongoose.model('Comment');
 router.get('/', function(req, res, next) {
   const {q} = req.query;
   const exp = new RegExp(q, 'i');
-    const query = {
-      $or: [
-        {slug: exp},
-        {title: exp},
-        {description: exp},
-        {body: exp}
-      ]
-    };
-  Article.find(query)
-    .sort({createdAt: 'desc'})
-    .populate('author')
-    .exec((error, docs) => {
-      if (error) {
-        res.json({
-          error: error.message
-        });
+  const query = {
+    $or: [
+      {slug: exp},
+      {title: exp},
+      {description: exp},
+      {body: exp}
+    ]
+  };
+  const limit = 20;
+  Article.count(query)
+    .then((count) => {
+      console.log(21, count);
+      if (count > 0) {
+        Article.find(query)
+          .limit(limit)
+          .sort({createdAt: 'desc'})
+          .populate('author')
+          .exec((error, docs) => {
+            if (error) {
+              res.json({
+                error: error.message
+              });
+            } else {
+              res.json({
+                total: count,
+                articles: docs.map(article => article.toJSONFor(article.author))
+              });
+            }
+          });
       } else {
         res.json({
-          articles: docs.map(article => article.toJSONFor(article.author))
+          total: count,
+          articles: []
         });
       }
-    });
+  });
 });
 
 module.exports = router;
